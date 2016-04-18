20919a0f4c9b3d0d0f9c80915de348a14a8dfd66
Lutz Behnke <lutz.behnke@gmx.de>
2016-04-18 20:25:23 +0200

    WIP: make more configurable

--- src/cucumberl_cli.erl
+++ src/cucumberl_cli.erl
@@ -10,6 +10,8 @@
 
 main(_) ->
     code:add_pathz("ebin"),
+    code:add_pathz("../ebin"),
+              
     Features = find_files("features", ".*\\.feature\$"),
     Outcomes = [ run_feature(F) || F <- Features ],
     case lists:all(fun(X) -> X =:= ok end, Outcomes) of
@@ -19,16 +21,23 @@ main(_) ->
 
 run_feature(FeatureFile) ->
     %% is there a mod *named* for this feature?
-    StepMod = ensure_loaded(list_to_atom(filename:basename(FeatureFile,
-                                                           ".feature"))),
-    {ok, #cucumberl_stats{failures=Failed}} =
-        cucumberl:run(FeatureFile, StepMod),
-    case Failed of
-        [] ->
-            ok;
-        _ ->
-            io:format("~p failed steps.~n", [length(Failed)]),
-            {failed, Failed}
+    StepModName = step_mod_name(FeatureFile),
+    StepMod = (catch ensure_loaded(StepModName)),
+    case StepMod of
+        {error, nofile} -> 
+            io:format("no module ~p found for feature ~p~n", [StepModName, FeatureFile]),
+            {failed, []};
+        
+        _Other          -> 
+            {ok, #cucumberl_stats{failures=Failed}} =
+                cucumberl:run(FeatureFile, StepMod),
+            case Failed of
+                [] ->
+                    ok;
+                _ ->
+                    io:format("~p failed steps.~n", [length(Failed)]),
+                    {failed, Failed}
+            end
     end.
 
 ensure_loaded(Mod) when is_atom(Mod) ->
@@ -41,3 +50,19 @@ ensure_loaded(Mod) when is_atom(Mod) ->
 
 find_files(Dir, Regex) ->
     filelib:fold_files(Dir, Regex, true, fun(F, Acc) -> [F | Acc] end, []).
+
+-define(STEPMOD_PREFIX, "sm").
+
+%% step_mod_name/1
+%%
+%% @doc generate unique and valid name for step module from path and name of feature file.
+%%
+-spec step_mod_name(FeatureFile :: file:filename()) -> atom().
+step_mod_name(FeatureFile) ->
+    %% should work, as the main will only run in the presence of a directory called 'features'
+    ["features"| DirElems] = filename:split(filename:dirname(FeatureFile)),
+    
+    Elems = [?STEPMOD_PREFIX] ++ DirElems ++ [filename:basename(FeatureFile, ".feature")],
+    
+    list_to_atom(string:to_lower(string:join(Elems, "_"))).
+
